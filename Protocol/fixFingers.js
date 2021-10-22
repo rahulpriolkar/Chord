c9f0cf38609fdb0c27e54fb7480e23852a590571
rahulpriolkar <rahulpriolkar01@gmail.com>
2021-10-22 13:47:37 +0530

    Added code to correctly update the finger table entries
    - fixFingers.js calls findSuccessor.js to get the successors
      which is scheduled using fixFingersScheduler.js
    - edge cases on the network boundary are also handled,
      using a modulo 2^(FINGER_TABLE_SIZE)

--- Protocol/fixFingers.js
+++ Protocol/fixFingers.js
@@ -1,23 +1,25 @@
 const bigInt = require('big-integer');
+const sha1 = require('sha1');
 
 const fixFingers = function () {
     return new Promise((resolve, reject) => {
+        const messageId = sha1(Date.now());
+
         this.send({
             nextNode: { ip: this.IP_ADDRESS, port: this.PORT, nodeId: this.NODE_ID },
             type: 'find-successor',
             params: {
                 startNode: this.getNodeInfo(),
-                id: bigInt(this.NODE_ID).add(Math.pow(2, this.fingerIndex)),
-                hopCount: 0
+                id: bigInt(this.NODE_ID).add(Math.pow(2, this.fingerIndex)).mod(Math.pow(2, this.FINGER_TABLE_SIZE)),
+                hopCount: 0,
+                messageId
             }
         });
 
-        if (this.PORT == 3000) console.log('fix-mid');
-
-        this.receive({ type: 'find-successor-response' }).then(({ successor, predecessor, hopCount }) => {
+        const event = `find-successor-response:${messageId}`;
+        this.ee.once(event, (successor) => {
             this.fingerTable[this.fingerIndex] = successor;
             this.fingerIndex = (this.fingerIndex + 1) % this.FINGER_TABLE_SIZE;
-            console.log('end-fix');
             resolve();
         });
     });
